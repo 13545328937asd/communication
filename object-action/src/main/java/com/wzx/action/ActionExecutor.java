aee9da4f72118826554a23ba45ee44b7968e4c20
wzx <245211464@qq.com>
2021-11-28 18:30:30 +0800

    no message

--- object-action/src/main/java/com/wzx/action/ActionExecutor.java
+++ object-action/src/main/java/com/wzx/action/ActionExecutor.java
@@ -10,6 +10,10 @@ import java.util.List;
  *      举例如下：①是否记住本次行为是谁让执行的。
  *                ②是否记住本次行为的返回结果。
  *                .......等
+ *
+ *  行为和拦截器的理解，行为是自己做的事情，拦截器是别人做的操作。
+ *  自己分内的事情，尽量不要让别人拦截去处理。比如 打印日志 更偏向于拦截处理，因为
+ *  拿到日志是管理员做日志分析和排查问题的。
  */
 public class ActionExecutor {
 
@@ -24,8 +28,6 @@ public class ActionExecutor {
                 Object actionResult = action.execute(actionContext);
 
                 //do something after the action execute
-
-
 
             }catch (Exception e){
                 //执行action报错之后的行为
